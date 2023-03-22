e2f00810623c28f3575564c3c6103956d2528d9c
unknown <you@example.com>
2023-03-21 23:16:08 -0600

    Actividad_6_Programación_I

---
+++ Punteros Dina.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+main (){
+	/*int edad,*p_edad;
+	p_edad=&edad;
+	cout<<"Ingrese edad: ";
+	cin>>edad;
+	cout<<*p_edad<<endl;
+	
+	if(*p_edad>18){
+		cout<<"Mayor de edad";
+	}else
+	{
+		cout<<"Menor de edad";
+	}*/
+	
+	//Por Medio de ARREGLOS
+	/*int total = 0;
+	int notas[total];
+	char res;
+	int *p_notas = notas;
+	do{
+		cout<<"Ingrese nota: "<<total<<": "; 
+		cin>>notas[total];
+		total++;
+		cout<<"Desea Ingresar otro Valor (s/n): "; cin>>res;
+	}while(res=='s'||res=='S');
+	
+	cout<<"----------- Mostrar notas ----------"<<endl;
+	for(int i;i<total;i++){
+		cout<<"Nota("<<i<<"): "<<*p_notas<<endl;
+		p_notas++;
+	}*/
+	
+	//Punteros con asignacionn de memoria dinamica
+	//new = reservar un espacio en memoria
+	//delete [] = Liberar La memoria
+	
+	//puntero unidemensional
+	/*int total = 0,*p_notas;
+	p_notas=new int[total];
+	char res;
+
+	do{
+		cout<<"Ingrese nota: "<<total<<": "; 
+		cin>>p_notas[total];
+		total++;
+		cout<<"Desea Ingresar otro Valor (s/n): "; cin>>res;
+	}while(res=='s'||res=='S');
+	
+	cout<<"----------- Mostrar notas ----------"<<endl;
+	for(int i;i<total;i++){
+		cout<<"Nota("<<i<<"): "<<*p_notas<<endl;
+		p_notas++;
+	}
+	delete[] p_notas;*/
+	
+	//Puntero bidimensional
+	int fil=0, col=0, **pm_notas;
+	cout<<"Ingrese la cantidad de Estudiantes: ";
+	cin>>fil;
+	cout<<"Ingrese la cantidad de notas por estudiante: ";
+	cin>>col;
+	
+	pm_notas=new int *[fil];
+	for(int i=0;i<fil;i++){
+		pm_notas[i] = new int[col];
+	 }
+	 for (int i=0;i<fil;i++){
+	 	 cout<<"_________Estudiante_________"<<i<<endl;
+	 	for(int ii=0;ii<col;ii++){
+	 		cout<<"Nota:"<<ii<<":";
+	 		cin>>*(*(pm_notas+i)+ii);
+	 		
+		 }
+		 cout<<"__________________"<<endl;
+	 }
+	 cout<<"-------------- Mostrar Notas -----------"<<endl;
+	  for (int i=0;i<fil;i++){
+	  	
+	 	for(int ii=0;ii<col;ii++){
+	 		cout<<*(*(pm_notas+i)+ii)<<endl;
+	 		
+		 }
+	  cout<<"__________________________"<<endl;	 
+	 }
+
+    for(int i=0;i<fil;i++){
+        delete [] pm_notas[i];
+    }
+    delete[] pm_notas;
+	system("pause");
+}
---
+++ Punteros.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+main (){
+	int edad=25,*p_edad;
+	p_edad = &edad;
+	cout<<"Variable Edad: "<<edad<<endl;
+	cout<<"Puntero Edad: "<<*p_edad<<endl;
+	//cout<<"Puntero Edad: "<<p_edad<<endl;
+	cout<<"-------- Cambiar Valores---------"<<endl;
+	*p_edad = 40;
+	cout<<"Variable Edad: "<<edad<<endl;
+	cout<<"Puntero Edad: "<<*p_edad<<endl;
+
+	cout<<"-------- Otra Variable a p_edad Valores---------"<<endl;
+	int edad2 = 100;
+	p_edad = &edad2;
+	edad=400;
+	cout<<"Variable Edad: "<<edad<<endl;
+	cout<<"Puntero Edad: "<<*p_edad<<endl;
+	cout<<"Variable Edad2: "<<edad2<<endl;
+	system("pause");
+}
